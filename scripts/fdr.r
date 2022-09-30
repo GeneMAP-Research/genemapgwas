1d8f10ea18f603652d91a3194bd19f81b7018925
esohkevin <esohkevin4@gmail.com>
2022-09-30 13:30:50 +0200

    add parameter

--- scripts/fdr.r
+++ scripts/fdr.r
@@ -2,8 +2,8 @@
 
 args <- commandArgs(TRUE)
 
-if (length(args) < 2) {
-        message("\nUsage: fdr.r [input] [p-value column name]\n")
+if (length(args) < 3) {
+        message("\nUsage: fdr.r [input] [p-value column name] [threads]\n")
         message("\tinput is association test result file with a p-value column\n")
 	quit(save="no")
 } else {
@@ -12,19 +12,20 @@ if (length(args) < 2) {
     }
    assoc_dat <- args[1]
    pval <- args[2]
+   threads <- as.numeric(args[3])
 
    calc_fdr <- function(assoc.dat = assoc_result) {
 	
-	out_name <- paste(assoc.dat,".adj.txt", sep="")
+	out_name <- paste(assoc.dat,".adjusted.txt.gz", sep="")
 
-	assoc <- fread(assoc.dat, header=T, data.table=F, nThread=10, fill=T)
+	assoc <- fread(assoc.dat, header=T, data.table=F, nThread = threads, fill=T)
 	assoc$p.BH <- p.adjust(as.vector(assoc[,pval]), method="BH")		# Benjamini & Hochberg
 	#assoc$P_adj_Bonf <- p.adjust(as.vector(assoc$P), method="bonferroni")	# Bonferroni
 	#assoc$P_adj_BY <- p.adjust(as.vector(assoc$P), method="BY")   		# Benjamini & Yekutieli
 	#assoc$P_adj_Holm <- p.adjust(as.vector(assoc$P), method="holm")   	# Holm
 	#assoc$P_adj_Hommel <- p.adjust(as.vector(assoc$P), method="hommel")  	# Hommel
 	#assoc$P_adj_Hochberg <- p.adjust(as.vector(assoc$P), method="hochberg") # Hochberg
-	write.table(assoc, file=out_name, col.names=T, row.names=F, quote=F, sep="\t")
+	fwrite(assoc, file=out_name, col.names=T, row.names=F, quote=F, sep="\t", nThread = threads)
 }
 calc_fdr(assoc_dat)
 }
