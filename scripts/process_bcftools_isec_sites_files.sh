a674a541485aedf2d64436fd3a8eeb75d0be9245
esohkevin <esohkevin4@gmail.com>
2022-10-04 11:25:58 +0200

    update

--- scripts/process_bcftools_isec_sites_files.sh
+++ scripts/process_bcftools_isec_sites_files.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-gzip -f sites.txt
-zcat sites.txt.gz | awk '{print $1,$2,$3,$4,"\""$5"\""}' | sed '1 i chrom pos ref alt info' | gzip -c > sites.2.txt.gz
+[ -e "sites.txt" ] && gzip -f sites.txt
+[ -e "sites.txt.gz" ] && zcat sites.txt.gz | awk '{print $1,$2,$3,$4,"\""$5"\""}' | sed '1 i chrom pos ref alt info' | gzip -c > sites.2.txt.gz
