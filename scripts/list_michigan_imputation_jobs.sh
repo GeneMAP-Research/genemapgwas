b51b370032dc7ef2c4a462321b7af3707b738a1b
esohkevin <esohkevin4@gmail.com>
2022-12-16 23:18:10 +0200

    update

--- scripts/list_michigan_imputation_jobs.sh
+++ scripts/list_michigan_imputation_jobs.sh
@@ -4,3 +4,9 @@ curl \
    -H "X-Auth-Token: $TOKEN" https://imputationserver.sph.umich.edu/api/v2/jobs | \
    jq . \
    > michigan_imputation_server_jobs.json
+
+echo ""
+echo "Job IDs sorted by date and time with most recent at the top"
+echo ""
+cat michigan_imputation_server_jobs.json | \
+   jq '.data[].id'
