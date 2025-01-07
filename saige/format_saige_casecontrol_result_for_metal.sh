d09a78fb9fb6bc1922c1e5c5f8acbd0b2e05e0b8
esohkevin <esohkevin4@gmail.com>
2025-01-06 22:26:53 -0500

    remove scripts

--- saige/format_saige_casecontrol_result_for_metal.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# -lt 1 ]; then
-    echo "Usage: format_saige_casecontrol_result_for_metal.sh [saige-casecontrol-result]"
-else
-    assoc=$1
-    awk '$15=="true" {print $3,$4,$5,$9,$10,$13,$18+$19}' ${assoc} | sed '1 i SNP A1 A2 BETA SE P NMISS' | sed 's/ /\t/g' > ${assoc}.metal
-fi
